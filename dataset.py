dca14734b00b8e96bd369175423bc980efc8251a
Vaibhav Bhargava <vbhar16@outlook.com>
2023-03-20 23:31:26 -0500

    Implemented __getitem__ for VWSDDataset class.

--- dataset.py
+++ dataset.py
@@ -29,7 +29,9 @@ import torch
 from enum import Enum
 import pandas as pd
 from torch.utils.data import Dataset
-import torchvision
+from torchvision.io import read_image
+from transformers.image_processing_utils import BaseImageProcessor
+from transformers.tokenization_utils_fast import PreTrainedTokenizerFast
 
 
 class DatasetConfig(Enum):
@@ -39,14 +41,18 @@ class DatasetConfig(Enum):
 
 
 class VWSDDataset(Dataset):
-    def __init__(self, img_data_path: str, wsd_data_path: str, gold_data_path: str, config: DatasetConfig, seed=42,
-                 split=0.8):
+    def __init__(self, img_data_path: str, wsd_data_path: str, gold_data_path: str, config: DatasetConfig,
+                 image_processor: BaseImageProcessor, tokenizer: PreTrainedTokenizerFast, seed=42, split=0.8):
         self.img_data_path = img_data_path
         self.wsd_data_path = wsd_data_path
         self.gold_data_path = gold_data_path
         self._config = config
         self.seed = seed
         self.split = split
+        self.image_processor = image_processor
+        self.fast_tokenizer = tokenizer
+        self.sep = '[CONDITIONED]'
+        self.fast_tokenizer.add_tokens(new_tokens=self.sep, special_tokens=False)
 
         columns = ['word', 'context'] + [f'{a}{idx}' for idx, a in enumerate(['sample'] * 10, start=1)]
         df = pd.read_csv(self.wsd_data_path, sep='\t', names=columns)
@@ -84,9 +90,40 @@ class VWSDDataset(Dataset):
 
     def __getitem__(self, item):
         # 1. Using purely transformers Vision Text Dual Encoder.
-        # 2. Base classing Vision Text Dual Encoder so that we can override it's forward method to pass embeddings.
-        # Assuming we have Images / ConNextv2 embeddings
-        data_point = self.df.iloc[item]
+        # 2. Base classing Vision Text Dual Encoder so that we can override its forward method to pass embeddings.
+        # Assuming we have Images / ConNextv2 embeddings.
+        sampled_idx = self.sampling_idx[item]
+
+        data_point = self.df.iloc[sampled_idx]
         word, context = data_point[:2]
 
+        # TODO: Need to see what shall be the separating token between the word and context if any.
+        text = f'{word}{self.sep}{context}'
 
+        # Tokenizer gives a dictionary of input_ids, token_type_ids, attention_mask.
+        text_input = self.fast_tokenizer(text, return_tensors='pt')
+
+        # TODO: Current Mining Scheme is only going as per the dataset.
+        #  We can go for more negative image examples based on the "word" not conditioned on "context".
+
+        # image processor gives a dictionary of pixel_values.
+        # imgs_input = [self.image_processor(img, return_tensors='pt')['pixel_values'] for img in imgs]
+        img_paths = data_point[2:] if self._config == DatasetConfig.TEST else data_point[2:-1]
+
+        imgs = [
+            self.image_processor(read_image(os.path.join(self.img_data_path, img_path)), return_tensors='pt')
+            for img_path in img_paths
+        ]
+
+        gold_example = torch.zeros((1, len(imgs)))
+
+        if self._config != DatasetConfig.TEST:
+            gold_path = data_point[-1]
+            for img_path in img_paths:
+                if img_path == gold_path:
+                    gold_example[:, img_path] = 1.0
+                    break
+
+    # TODO: Testing whether this scales to batching in dataloader.
+
+        return text_input, imgs, gold_example
