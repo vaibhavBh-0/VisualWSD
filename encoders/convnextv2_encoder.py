db7b86ffcfe080f7a44c923c2ef04185bdd64d6e
Vaibhav Bhargava <vbhar16@outlook.com>
2023-03-21 00:01:57 -0500

    Created the encoder package.

---
+++ encoders/convnextv2_encoder.py
@@ -0,0 +1,42 @@
+# convnextv2_encoder.py
+#
+# Created by Vaibhav Bhargava on 20-03-2023
+# 
+# Copyright © 2023 Vaibhav Bhargava
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
+import torch.nn as nn
+from transformers import ConvNextV2Model
+
+
+class ConvNextV2Encoder(nn.Module):
+    def __init__(self, embedding_dim: int, model_path: str):
+        super(ConvNextV2Encoder, self).__init__()
+
+        self.conv_next_model = ConvNextV2Model.from_pretrained(model_path)
+        in_dim = list(self.conv_next_model.parameters())[-1].shape[0]
+        self.embedding_dim = nn.Linear(in_dim, embedding_dim)
+        
+    def forward(self, x):
+        x = self.conv_next_model(x)
+        x = self.embedding_dim(x)
+
+        return x
